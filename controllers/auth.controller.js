a17bb615651775851454c08c27cb021230e4cc48
macbookpro <amulyadubey99@gmail.com>
2021-11-06 16:21:32 +0530

    social login included

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,3 +48,27 @@ exports.requireSignin = expressJwt({
   userProperty: "auth",
   algorithms: ["HS256"],
 });
+
+exports.socialLogin = (req, res) => {
+  let user = User.findOne({ email: req.body.email }, (err, user) => {
+    if (err || !user) {
+      // create a new user and login
+      user = new User(req.body);
+      req.profile = user;
+      user.save();
+    } else {
+      // update existing user with new social info and login
+      req.profile = user;
+      user = _.extend(user, req.body);
+      user.updated = Date.now();
+      user.save();
+    }
+    const token = jwt.sign(
+      { _id: user._id, iss: "NODEAPI" },
+      process.env.JWT_SECRET
+    );
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    const { _id, name, email } = user;
+    return res.json({ token, user: { _id, name, email } });
+  });
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { signin, signup, signout } = require("../controllers/auth.controller");
+const { signin, signup, signout, socialLogin } = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/social-login", socialLogin);
 router.get("/signout", signout);
-
-// any route containing :userId, our app will first execute userByID()
-// router.param("userId", userById);
 
 module.exports = router;
